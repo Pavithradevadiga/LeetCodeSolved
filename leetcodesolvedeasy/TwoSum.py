f7e2d693b3a66e9ed592a9100e4721f08baabea4
pavithradevadiga <pavithradevadiga1@gmail.com>
2023-01-17 17:04:37 +0530

    Added twosum optimized solution

--- leetcodesolvedeasy/TwoSum.py
+++ leetcodesolvedeasy/TwoSum.py
@@ -1,9 +1,22 @@
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        print(nums)
+        print("LazySolution:")
         indexOfValue = 1
         for i,val in enumerate(nums):
             for j in range(i+1,len(nums)):
                 if val+nums[j] == target :
                     print(str(val)+"+"+str(nums[j])+",=,"+str(target)+","+str(i)+","+str(j))
                     return [i,j]
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        print("OptimizedSolution:")
+        trackedValues = {}
+        for i,value in enumerate(nums):
+            difference = target - value
+            if difference in trackedValues:
+                return [trackedValues[difference],i]
+            trackedValues[value] = i                     
